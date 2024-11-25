fcdce5b4c9c5c791d9b54e06c9b8ccd14a57fba6
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-11-25 15:07:07 -0500

    Error handling in ChatBubble:init

--- scripts/objects/online/ChatBubble.lua
+++ scripts/objects/online/ChatBubble.lua
@@ -6,19 +6,34 @@ function ChatBubble:init(actor, text, x, y)
     self.origin_x = 0.5
     local x_offset = actor.width * 2
     local y_offset = -actor.height
-    self.text = Text(text, x_offset, y_offset, SCREEN_WIDTH * 2, nil, {
+    local ok
+    ok, self.text = pcall(Text, text, x_offset, y_offset, SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, {
         align = "center",
         font = "main_mono"
     })
-    self.width, self.height = self.text:getSize()
-    self.text.width, self.text.height = self.text:getSize() -- Freeze!
+    if (not ok) or (({self.text:getSize()})[1] == 0) then
+        text = "[color:red](Invalid)"
+        self.text = Text(text, x_offset, y_offset, SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, {
+            align = "center",
+            font = "main_mono"
+        })
+    end
+    local w,h = self.text:getSize()
+    self.text.width, self.text.height = math.max(4, w), math.max(4, h)
+    self.width, self.height = math.max(4, w), math.max(4, h)
     self.text:setText(text)
     self.lifetime = 11
     self.rectangle = Rectangle(x_offset, y_offset, self.text:getSize())
     self:addChild(self.rectangle)
     self:addChild(self.text)
+    self.text.debug_select = false
+    self.rectangle.debug_select = false
     self.rectangle.alpha = 0.5
     self.rectangle.color = {0.2,0.2,0.2}
+end
+
+function ChatBubble:getDebugRectangle()
+    return {0, 0, self.text:getSize()}
 end
 
 function ChatBubble:update()
